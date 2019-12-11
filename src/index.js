a2a4330341bb93aaca9aef40055a5a4e611e03df
HadiKAR <hadikurniawanar@gmail.com>
2019-12-11 14:29:38 +0700

    just adding comment

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 // 3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
 // 4. Add a toggle button that lets you sort the moves in either ascending or descending order. (10Dec'19)
 // 5. When someone wins, highlight the three squares that caused the win. (11Dec'19)
+// 6. When no one wins, display a message about the result being a draw. (11Dec'19)
 
 class Square extends React.Component {
   render() {
